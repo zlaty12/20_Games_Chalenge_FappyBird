5c8337ad978db8848f18420504add3892c0c6f26
Zlatimir Rangelov <94063648+zlaty12@users.noreply.github.com>
2026-07-09 07:42:33 +0100

    RayLib Widow init

--- FlappyBird/Main.cpp
+++ FlappyBird/Main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include <raylib.h>
 
+const int ScreenWidth = 1600;
+const int ScreenHeigth = 900;
+
+
+
+
 int main()
 {
-	std::cout << "Hello RayLib" << std::endl;
+	InitWindow(ScreenWidth, ScreenHeigth, "20 Games Chalenge Flappy Bird");
+	SetTargetFPS(60);
+
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
 }
