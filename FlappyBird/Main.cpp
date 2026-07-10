8ea8601e9b54ecaa653d02618e5bda4dc7caed24
Zlatimir Rangelov <94063648+zlaty12@users.noreply.github.com>
2026-07-10 06:28:51 +0100

    Input
    Added gravity and jump input.

--- FlappyBird/Main.cpp
+++ FlappyBird/Main.cpp
@@ -7,6 +7,45 @@ const int ScreenHeight = 900;
 const int BirdHight = 70;
 const int BrdWidth = 70;
 
+const float FallingSpeed = 50.f;
+
+Vector2 BirdStartPosition = Vector2(ScreenWidht / 2.f - 500.f, ScreenHeight / 2.f);
+
+class Pipe
+{
+
+public:
+	Vector2 PiplePositionTop;
+	Vector2 PipePositionBottom;
+
+	// add two pipe draws
+	//DrawRectangle(Position.x, Position.y, BrdWidth, BirdHight, RED);
+	//DrawRectangle(Position.x, Position.y, BrdWidth, BirdHight, RED);
+};
+
+
+class Bird
+{
+	
+	
+	
+
+public:
+	bool bCanJump;
+	Vector2 Position;
+
+	Bird(Vector2 Position)
+		: Position(Position)
+	{
+		
+	}
+
+
+	void DrawBird()
+	{
+		DrawRectangle(Position.x, Position.y, BrdWidth, BirdHight, RED);
+	}
+};
 
 
 int main()
@@ -14,14 +53,24 @@ int main()
 	InitWindow(ScreenWidht, ScreenHeight, "20 Games Chalenge Flappy Bird");
 	SetTargetFPS(60);
 
+	Bird PlayerBird = Bird(BirdStartPosition);
 
+
+	
 	while (!WindowShouldClose())
 	{
+
 		BeginDrawing();
-
-		DrawRectangle(ScreenWidht / 2.f - 500, ScreenHeight / 2.f, BrdWidth, BirdHight, RED);
-
+		
 		ClearBackground(RAYWHITE);
+
+		PlayerBird.DrawBird();
+
+		if (IsKeyPressed(KEY_SPACE)) PlayerBird.Position.y += -50.f;
+
+		PlayerBird.Position.y += FallingSpeed * GetFrameTime();
+
+		
 
 		EndDrawing();
 	}
